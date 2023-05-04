b649e74b41bf9c1821e1f10d85c4ca7e2d70910d
DeTanvir <tanvirahmed14.ta@gmail.com>
2023-05-04 12:34:16 +0600

    last commit for Module-64

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -41,17 +41,17 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
+                loader: ({params}) => fetch(`https://the-news-dragon-server-detanvir.vercel.app/categories/${params.id}`)
             },
             // {
             //     path: '/',
             //     element: <Category></Category>,
-            //     loader: () => fetch('http://localhost:5000/news')
+            //     loader: () => fetch('https://the-news-dragon-server-detanvir.vercel.app/news')
             // },
             // {
             //     path: '/category/:id',
             //     element: <Category></Category>,
-            //     loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
+            //     loader: ({params}) => fetch(`https://the-news-dragon-server-detanvir.vercel.app/categories/${params.id}`)
             // },
         ]
     },
@@ -62,7 +62,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <PrivateRoute><News></News></PrivateRoute>,
-                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({params}) => fetch(`https://the-news-dragon-server-detanvir.vercel.app/news/${params.id}`)
             },
         ]
     }
